f7012941848c7eb67f9d1f2c178121163273191a
unknown <adewaletheophilus@gmail.com>
2024-01-03 00:35:33 +0100

    Initial Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-teal-500 text-2xl font-semibold">MEMORY CARD</h1>
+    </div>
+  );
+}
+
+export default App;
